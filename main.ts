0b40244df27d362175f61095b34a83fbc7f64c4b
casabubu21 <87440460+casabubu21@users.noreply.github.com>
2021-10-12 20:37:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
 radio.onReceivedNumber(function (receivedNumber) {
     serial.writeValue("y", receivedNumber)
 })
-basic.showIcon(IconNames.Heart)
 radio.setGroup(33)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # .
+    # # # # .
+    . # . # .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
